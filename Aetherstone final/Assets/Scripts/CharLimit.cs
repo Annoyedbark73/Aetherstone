4862ccafdd0e8a2005151b8f2881e0d353d4b8b8
Annoyedbark73 <annoyedbark73@gmail.com>
2020-12-23 00:48:34 -0600

    Worked on setting playerName
    Dude its 12:47 rn idk what im doing welp
    Commit to main i guess i can always $git reset ~hard

---
+++ Aetherstone final/Assets/Scripts/CharLimit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharLimit : MonoBehaviour
+{
+    float Min = 1;
+    float Max = 20;
+    float NumberToClamp;
+    void Update()
+    {
+        NumberToClamp = Mathf.Clamp(NumberToClamp, Min, Max);
+    }
+}
--- Aetherstone final/Assets/Scripts/SavePlayerPosition.cs
+++ Aetherstone final/Assets/Scripts/SavePlayerPosition.cs
@@ -1,19 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SavePlayerPosition : MonoBehaviour
 {
-  public void SavePostion()
-    {
-        PlayerPrefs.SetFloat("PlayerX", player.transform.position.x);
-        PlayerPrefs.SetFloat("PlayerY", player.transform.position.y);
-        PlayerPrefs.SetFloat("PlayerZ", player.transform.position.z);
-    }
 
-    public void loadposition()
-    {
-        transform.position = new Vector3(PlayerPrefs.GetFloat("PlayerX"), PlayerPrefs.GetFloat("PlayerY"), PlayerPrefs.GetFloat("PlayerZ"));
-
-    }
 }
